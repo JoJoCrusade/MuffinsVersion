7c27fe4569f9dabefaf4be74c96c09b5fc0ddd1f
JC <jc@DESKTOP-4FS6JCR.localdomain>
2020-09-07 00:22:19 -0400

    starting mom script works pretty well. Needs a few extra detials and fixes. To do: Add Fisherwoman and start working on giving starter pokemon

--- include/constants/vars.h
+++ include/constants/vars.h
@@ -272,8 +272,9 @@
 #define VAR_UNUSED_0x40FD                    0x40FD // Unused Var
 #define VAR_UNUSED_0x40FE                    0x40FE // Unused Var
 #define VAR_UNUSED_0x40FF                    0x40FF // Unused Var
+#define VAR_SANDPEARL_WARPSET				 0X4100
 
-#define VARS_END                             0x40FF
+#define VARS_END                             0x4100
 #define VARS_COUNT                           (VARS_END - VARS_START + 1)
 
 #define SPECIAL_VARS_START            0x8000
